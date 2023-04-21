a341acb9d914f71d22a757fa63f32bd787ad352e
Thái <93896412+thaihx13834@users.noreply.github.com>
2023-04-21 13:34:20 +0700

    config_state

--- src/features/water/dataWaterSlice.tsx
+++ src/features/water/dataWaterSlice.tsx
@@ -16,6 +16,14 @@ export const getDataWater = createAsyncThunk('water/getDataWater', async () => {
   return data;
 });
 
+export const putDataWater = createAsyncThunk('water/putDataWater', async (dataUpdate: any) => {
+  const { data }: any = await axios.put(
+    `https://6440c167fadc69b8e071d4b4.mockapi.io/api/houses/${dataUpdate.id}`,
+    dataUpdate
+  );
+  return data;
+});
+
 export const waterSlice = createSlice({
   name: 'water',
   initialState,
@@ -24,9 +32,14 @@ export const waterSlice = createSlice({
     builder.addCase(getDataWater.fulfilled, (state, action) => {
       return void (state.value = action.payload);
     });
+    builder.addCase(putDataWater.fulfilled, (state, action) => {
+      return void (state.value = state.value.map((item: any) =>
+        item.id === action.payload.id ? action.payload : item
+      ));
+    });
   },
 });
 
 // Other code such as selectors can use the imported `RootState` type
-export const dataWaterAction = waterSlice.actions;
+// export const dataWaterAction = waterSlice.actions;
 export default waterSlice.reducer;
