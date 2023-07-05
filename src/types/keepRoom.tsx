de0d1970384190a453239e3a837d63c9d2b2e534
toaineee <buitoai2031@gmail.com>
2023-07-06 00:15:42 +0700

    feat: create keep room type

--- src/types/keepRoom.tsx
+++ src/types/keepRoom.tsx
@@ -1,5 +1,11 @@
 export type Keep_Room_Type = {
   _id?: number;
-  houseId: string,
-  
+  houseId: string;
+  roomId: string;
+  name: string;
+  phone: string;
+  money: number;
+  bookingDate: string;
+  checkInDate: string;
+  note: string;
 };
