53655726888ed73e39e6679134cb51dea430b087
Thái <93896412+thaihx13834@users.noreply.github.com>
2023-09-18 22:07:48 +0700

    fix

--- src/pages/admin/charge/Charge.tsx
+++ src/pages/admin/charge/Charge.tsx
@@ -742,9 +742,8 @@ const Charge = () => {
       // const data = await addBill(dataInput);
       dispatch(addCharge({ input: dataInput, filter: valueFilter }));
       form.resetFields();
-      toast.success('Thanh toán thành công!');
     } catch (error) {
-      toast.error('Thanh toán không thành công!');
+      toast.error('Tính tiền không thành công!');
     }
   };
 
